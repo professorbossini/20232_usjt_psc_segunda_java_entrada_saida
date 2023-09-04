dfdaa3f62836c005e0f8151e6816eec053b91fce
Rodrigo Bossini <professorbossini@gmail.com>
2023-09-04 19:49:11 -0300

    ap02(input-output): captura dois valores

--- SomaDoisNumeros.java
+++ SomaDoisNumeros.java
@@ -3,13 +3,16 @@ import java.util.Scanner;
 public class SomaDoisNumeros{
   public static void main(String [] args){
     //declaração de variáveis
-    Scanner leitor = new Scanner(System.ingit);
+    Scanner leitor = new Scanner(System.in);
     double primeiroOperando;
     double segundoOperando;
     double resultado;
 
     //entrada
-
+    System.out.println("Digite o primeiro operando");
+    primeiroOperando = leitor.nextDouble();
+    System.out.println("Digite o segundo operando");
+    segundoOperando = leitor.nextDouble();
     //processamento
 
     //saída
